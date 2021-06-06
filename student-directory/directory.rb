12eb5170343ae8cfebde475ea50f8368c82cd463
Juel07 <juel.braganza97@gmail.com>
2021-06-06 23:07:17 +0100

    change array to hash

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,16 +1,16 @@
 # let's put all students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates",
+  { name: "Dr. Hannibal Lecter", cohort: :november },
+  { name: "Darth Vader", cohort: :november },
+  { name: "Nurse Ratched", cohort: :november },
+  { name: "Michael Corleone", cohort: :november },
+  { name: "Alex DeLarge", cohort: :november },
+  { name: "The Wicked Witch of the West", cohort: :november },
+  { name: "Terminator", cohort: :november },
+  { name: "Freddy Krueger", cohort: :november },
+  { name: "The Joker", cohort: :november },
+  { name: "Joffrey Baratheon", cohort: :november },
+  { name: "Norman Bates", cohort: :november },
 ]
 
 def print_header
@@ -18,15 +18,19 @@ def print_header
   puts "-------------"
 end
 
-def print_names(names)
-  names.each do |student|
-    puts student
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-# We are passing the students variable to the methods as an argument (called "names" in both cases).
+# We are passing the students variable to the methods as an argument.
 # This is because the methods don't have access to local variables defined outside them.
+
+print_header
+print(students)
+print_footer(students)
